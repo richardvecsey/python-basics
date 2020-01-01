632ca005079067cd00678a9ffeb58f929c64200e
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-01-01 17:49:30 +0100

    Upload 003-input.py
    Get input from user via console or command line

---
+++ 003-input.py
@@ -0,0 +1,8 @@
+"""
+Get input from user via console or command line
+-----------------------------------------------
+Output:     (string)  
+"""
+
+name = input('What is your name? ')
+print('Hello {}!'.format(name))
