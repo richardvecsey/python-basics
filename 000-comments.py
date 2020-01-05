e384d0211683cc77e9fbeec9472aa37a8fc50d1e
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-01-05 18:18:56 +0100

    Upload 000-comments.py
    Present commenting methods

---
+++ 000-comments.py
@@ -0,0 +1,17 @@
+"""
+Present commenting methods
+"""
+
+# This is a comment
+
+# This is the first line of comment
+# And this is the second one
+
+"""
+You can write multi line comments between three-quotation marks
+"""
+
+print('This will be printed!') # but this comment won't
+
+# Remember, part of code in comment won't be executed
+# print('This message won't be printed!')
