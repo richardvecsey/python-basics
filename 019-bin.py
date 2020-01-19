283bd7932a5f6a6f76541040112d5f073c6382bb
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-01-19 19:48:29 +0100

    Upload 019-bin.py
    Return the binary version of any integer

---
+++ 019-bin.py
@@ -0,0 +1,20 @@
+"""
+Return the binary version of any integer
+----------------------------------------
+
+Input:      (int)  any integer
+
+Return:     binary version of integer
+"""
+
+int_1 = 1
+print('Integer: {}\nBinary version: {}'.format(int_1, bin(int_1)))
+
+int_2 = 10
+print('\nInteger: {}\nBinary version: {}'.format(int_2, bin(int_2)))
+
+int_3 = -10
+print('\nInteger: {}\nBinary version: {}'.format(int_3, bin(int_3)))
+
+int_4 = 0
+print('\nInteger: {}\nBinary version: {}'.format(int_4, bin(int_4)))
