325b9a8c58f417925862656cfd6a5cbdb90be6b2
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-01-23 23:37:34 +0100

    fix: change texts in print function
    "Boolean" correct to "All"

--- 021-all.py
+++ 021-all.py
@@ -20,24 +20,24 @@ print('\nObject: {}\nAll: {}'.format(empty_list, all_empty_list))
 # Tuple
 test_tuple = (0, 0, 1)
 all_test_tuple = all(test_tuple)
-print('\nObject: {}\nBoolean: {}'.format(test_tuple, all_test_tuple))
+print('\nObject: {}\nAll: {}'.format(test_tuple, all_test_tuple))
 
 # Tuple 2
 test_tuple_2 = (25, 25, 25)
 all_test_tuple_2 = all(test_tuple_2)
-print('\nObject: {}\nBoolean: {}'.format(test_tuple_2, all_test_tuple_2))
+print('\nObject: {}\nAll: {}'.format(test_tuple_2, all_test_tuple_2))
 
 # Dictionary
 dictionary = {"name": "Alice", "age": "30"}
 all_dictionary = all(dictionary)
-print('\nObject: {}\nBoolean: {}'.format(dictionary, all_dictionary))
+print('\nObject: {}\nAll: {}'.format(dictionary, all_dictionary))
 
 # Dictionary 2
 dictionary_2 = {"name": True, "age": False}
 all_dictionary_2 = all(dictionary_2)
-print('\nObject: {}\nBoolean: {}'.format(dictionary_2, all_dictionary_2))
+print('\nObject: {}\nAll: {}'.format(dictionary_2, all_dictionary_2))
 
 # Dictionary 3
 dictionary_3 = {0: "Alice", 1: "name"}
 all_dictionary_3 = all(dictionary_3)
-print('\nObject: {}\nBoolean: {}'.format(dictionary_3, all_dictionary_3))
+print('\nObject: {}\nAll: {}'.format(dictionary_3, all_dictionary_3))
