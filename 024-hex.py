d6e2f925f2e3434fb3d656afcadfa8a1b2c96279
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-01-26 14:16:37 +0100

    Upload 024-hex.py
    Return the hexadecimal version of any integer

---
+++ 024-hex.py
@@ -0,0 +1,24 @@
+"""
+Return the hexadecimal version of any integer
+---------------------------------------------
+
+Input:      (int)   any integer
+
+Return:     (hex)   hexadecimal version of integer
+                    return always begins with '0x'
+"""
+
+int_1 = 1
+print('Integer: {}\nHexadecimal version: {}'.format(int_1, hex(int_1)))
+
+int_2 = 20
+print('\nInteger: {}\nHexadecimal version: {}'.format(int_2, hex(int_2)))
+
+int_3 = -20
+print('\nInteger: {}\nHexadecimal version: {}'.format(int_3, hex(int_3)))
+
+int_4 = 0
+print('\nInteger: {}\nHexadecimal version: {}'.format(int_4, hex(int_4)))
+
+int_5 = 16
+print('\nInteger: {}\nHexadecimal version: {}'.format(int_5, hex(int_5)))
