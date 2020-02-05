1dfdb72bca66ff949cd0661a4e4db01ecbcf290d
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-05 23:43:53 +0100

    feat: calc mean without function, style: print
    feat: calculate mean without function
    style: add linebreaks to print

--- 032-mean.py
+++ 032-mean.py
@@ -11,7 +11,13 @@ numbers = [10, 5, 0, -5, -10]
 mean_1 = mean(numbers)
 print('original numbers: {}\nmean: {}'.format(numbers, mean_1))
 
-
 numbers_2 = [10.0, 5.0, 0, -5.0, -10]
 mean_2 = mean(numbers_2)
-print('original numbers: {}\nmean: {}'.format(numbers_2, mean_2))
+print('\noriginal numbers: {}\nmean: {}'.format(numbers_2, mean_2))
+
+# How to calculate arithmetic mean without satistics.mean() function?
+numbers_3 = [20.0, 10, 0, -5.0, -10]
+sum_numbers = sum(numbers_3)
+count_numbers = len(numbers_3)
+mean_numbers = sum_numbers / count_numbers
+print('\noriginal numbers: {}\nmean without function: {}'.format(numbers_3, mean_numbers))
