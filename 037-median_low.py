5a17e97934ae849c558c560657380598c4f79d6d
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-10 23:30:55 +0100

    upload 037-median_low.py
    Calculate median of numbers

---
+++ 037-median_low.py
@@ -0,0 +1,24 @@
+"""
+Calculate median of numbers
+---------------------------
+Input:      (list) numbers
+
+Output:     (number) median of numbers
+                     whether the data is odd, return value is the middle number
+                     whether the data is even, return value is the smaller of the
+                     two middle numbers
+"""
+from statistics import median_low
+
+
+numbers = [20, 10, 5]
+median_1 = median_low(numbers)
+print('original numbers: {}\nmedian: {}'.format(numbers, median_1))
+
+numbers_2 = [20.0, 10.0, 5.0]
+median_2 = median_low(numbers_2)
+print('\noriginal numbers: {}\nmedian: {}'.format(numbers_2, median_2))
+
+numbers_3 = [1, 2, 3, 5]
+median_3 = median_low(numbers_3)
+print('\noriginal numbers: {}\nmedian: {}'.format(numbers_3, median_3))
