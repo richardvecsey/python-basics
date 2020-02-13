0bf2d02503376d860895eb7b61aea717b959a1aa
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-13 22:57:25 +0100

    040-del.py
    Delete variable or user-defined object

---
+++ 040-del.py
@@ -0,0 +1,43 @@
+"""
+Delete variable or user-defined object
+--------------------------------------
+Input:      (None)
+
+Output:     (None)
+
+Return:     (None)  Delete object or variable
+"""
+
+# Create a dummy list named cars, filled with brands from car industry
+cars = ["Audi", "BMW", "Chrysler", "Dodge"]
+
+print('Cars: {}'.format(cars))
+
+# Delete list named cars
+del cars
+
+try:
+    print('Cars: {}'.format(cars))
+except:
+    print('Variable named cars is deleted.\n')
+
+
+
+# Create a dummy class named CarsClass, contains a list named cars, filled
+# with brands from car industry
+class CarsClass():
+
+    cars = ["Audi", "BMW", "Chrysler", "Dodge"]
+
+    def function(self):
+        print('Cars: {}'.format(cars))    
+    
+print(CarsClass)
+
+# Delete class named CarsClass
+del CarsClass
+
+try:
+    print('Cars: {}'.format(CarsClass))
+except:
+    print('Class named CarsClass is deleted.\')
