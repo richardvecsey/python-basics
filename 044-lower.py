76ec3bdcba7f9f873a6393f42dfdc3d001296dab
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-17 23:00:57 +0100

    upload 044-lower.py
    Return lowercase version of a string

---
+++ 044-lower.py
@@ -0,0 +1,13 @@
+"""
+Return lowercase version of a string
+------------------------------------
+Input:      (string)    any string
+
+Output:     (string)    lowercase version of input string
+"""
+
+original_string = 'This is an "A" letter.'
+modified_string = original_string.lower()
+
+print('  Original string: {}\nLowercase version: {}'
+      .format(original_string, modified_string))
