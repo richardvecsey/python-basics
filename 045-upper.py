2d651364abd138e76d6f245c59850497b17102c3
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-18 22:03:15 +0100

    upload 045-upper.py
    Return uppercase version of a string

---
+++ 045-upper.py
@@ -0,0 +1,13 @@
+"""
+Return uppercase version of a string
+------------------------------------
+Input:      (string)    any string
+
+Output:     (string)    uppercase version of input string
+"""
+
+original_string = 'This is an "A" letter.'
+modified_string = original_string.upper()
+
+print('  original string: {}\nuppercase version: {}'
+      .format(original_string, modified_string))
