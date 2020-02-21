9b8241b38110f1514d6514ffdfb668b338aebca5
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-21 23:14:03 +0100

    upload 047-capitalize.py
    Return capitalized version of a string

---
+++ 047-capitalize.py
@@ -0,0 +1,13 @@
+"""
+Return capitalized version of a string
+--------------------------------------
+Input:      (string)    any string
+
+Output:     (string)    capitalized version of input string
+"""
+
+original_string = 'This Is An "A" Letter.'
+modified_string = original_string.capitalize()
+
+print('    original string: {}\ncapitalized version: {}'
+      .format(original_string, modified_string))
