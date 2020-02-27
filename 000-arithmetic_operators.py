9ca03d85f70fa173f3f761660b2cfca82dfc5dc9
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-02-27 23:29:25 +0100

    upload 000-arithmetic_operators.py
    Present arithmetic operators

---
+++ 000-arithmetic_operators.py
@@ -0,0 +1,34 @@
+"""
+Present arithmetic operators
+"""
+
+x = 7
+y = 3
+
+# Addition: +
+# x + y
+print('Addition: {} + {} = {}'.format(x, y, (x + y)))
+
+# Subtraction: -
+# x - y
+print('Subtraction: {} - {} = {}'.format(x, y, (x - y)))
+
+# Multiplication: *
+# x * y
+print('Multiplication: {} * {} = {}'.format(x, y, (x * y)))
+
+# Division: /
+# x / y
+print('Division: {} / {} = {}'.format(x, y, (x / y)))
+
+# Exponentiation: **
+# x ** y
+print('Exponentiation: {} ** {} = {}'.format(x, y, (x ** y)))
+
+# Floor division: //
+# x // y
+print('Floor division: {} // {} = {}'.format(x, y, (x // y)))
+
+# Modulus: %
+# x % y
+print('Modulus: {} % {} = {}'.format(x, y, (x % y)))
