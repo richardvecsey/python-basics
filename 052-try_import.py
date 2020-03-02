b2cc2d7a9849c75c8160ca4427c1db6c3cd89327
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-03-02 22:59:08 +0100

    feat: real life example in 052-try_import.py
    Add real life example with mean and fmean from statistics that show how to get speed up with python 3.8, but also work with python 3.x

--- 052-try_import.py
+++ 052-try_import.py
@@ -2,6 +2,10 @@
 Show example how to handle missing import
 -----------------------------------------
 This method is useful to handle missing import
+
+update 1: Add real life example with mean and fmean from statistics
+          It helps to create a code that runs faster with python 3.8, but also
+          works with python 3.x
 """
 
 failed = []
@@ -30,3 +34,29 @@ for i in success:
 
 for i in failed:
     print('{} failed'.format(i))
+    
+
+# Real life example
+# fmean() is fester but available only from python 3.8
+fmean_loaded = False
+
+# Try to load statistics.fmean
+try:
+    from statistics import fmean
+    fmean_loaded = True
+# If it fails, loaded statistics.mean
+except:
+    from statistics import mean
+
+# Two ways to calculate mean based on result of import
+numbers = [20, 5, 0, -5, -10]
+if fmean_loaded:
+    mean = fmean(numbers)
+else:
+    mean = mean(numbers)
+
+print('\nOriginal numbers: {}\nmean: {}'.format(numbers, mean))
+if fmean_loaded:
+    print('calculated by fmean()')
+else:
+    print('calculated by mean()')
