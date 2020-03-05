2e9d1a8d6b86498d5fb64df1b346f2cc86ebdd23
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-03-05 22:21:41 +0100

    upload 055-oct.py
    Return the octal value of any integer

---
+++ 055-oct.py
@@ -0,0 +1,30 @@
+"""
+Return the octal value of any integer
+-------------------------------------
+
+Input:      (int)   any integer
+
+Return:     (oct)   octal version of integer
+                    return always begins with '0o'
+"""
+
+int_1 = 1
+print('Integer: {}\nOctal value: {}'.format(int_1, oct(int_1)))
+
+int_2 = 20
+print('\nInteger: {}\nOctal value: {}'.format(int_2, oct(int_2)))
+
+int_3 = -20
+print('\nInteger: {}\nOctal value: {}'.format(int_3, oct(int_3)))
+
+int_4 = 0
+print('\nInteger: {}\nOctal value: {}'.format(int_4, oct(int_4)))
+
+int_5 = 16
+print('\nInteger: {}\nOctal value: {}'.format(int_5, oct(int_5)))
+
+int_6 = -8
+print('\nInteger: {}\nOctal value: {}'.format(int_6, oct(int_6)))
+
+int_7 = 7
+print('\nInteger: {}\nOctal value: {}'.format(int_7, oct(int_7)))
