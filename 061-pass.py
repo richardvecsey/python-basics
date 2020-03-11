d204be3d6df95ef47907073749441a6a0bdc2684
Richárd Ádám Vécsey Dr <46427561+richardvecsey@users.noreply.github.com>
2020-03-11 19:45:18 +0100

    upload 061-pass.py
    Use null statement as placeholder for future implementation

---
+++ 061-pass.py
@@ -0,0 +1,15 @@
+"""
+Use null statement as placeholder for future implementation
+-----------------------------------------------------------
+Python interpreter doesn't ignore pass. The difference between pass and comment,
+that interpreter ignores comments. Pass is unll statement that means it is a
+general palceholder for future implementation of functions, loops, classes.
+Using pass help avoid the runtime errors and the code will be runable.
+"""
+print('This is the beginning of example')
+
+# Using pass prevent running the for loop.
+for i in range(4):
+    pass
+
+print('\nEnd of examples')
